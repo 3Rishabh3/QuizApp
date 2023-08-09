73087afa26fcbea4421204a0ac96e5fd89306829
Rishabh Gupta <3rishabh3@gmail.com>
2023-08-10 01:31:50 +0530

    minor changes RANDOM function in sql query to get questions

--- question-service/src/main/java/com/rishabh/questionservice/dao/QuestionDao.java
+++ question-service/src/main/java/com/rishabh/questionservice/dao/QuestionDao.java
@@ -12,7 +12,7 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
 
-    @Query(value = "SELECT q.id FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    @Query(value = "SELECT q.id FROM question q WHERE q.category = :category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
     List<Integer> findRandomQuestionsByCategory(String category, int numQ);
 
     @Query(value = "SELECT q.right_answer FROM question q WHERE q.id=:id", nativeQuery = true)
